fb18820b5c9764d9e47d3047fe6a6096688139b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-02 15:09:23 +0300

    Added some db connection that runs at on app startup and disconnects on app shutdown

--- app/main.py
+++ app/main.py
@@ -1,7 +1,25 @@
 """The entrypoint fot the app"""
 from fastapi import FastAPI
+from database.database import database, sqlalchemy_engine
+from models.posts_model import metadata
 
 app = FastAPI()
+
+
+@app.on_event('startup')
+async def startup():
+    """Database connection code runs on startup
+    """
+    await database.connect()
+    metadata.create_all(sqlalchemy_engine)
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    """Database disconnection code runs on shutdown
+
+    """
+    await database.disconnect()
 
 
 @app.get('/', name="Root endpoint", description="An endpoint to test the app")
