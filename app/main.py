9e165849619004cf93d06e507df01211b8712b8f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-03 15:05:50 +0300

    Wrote the list_posts endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 """The entrypoint fot the app"""
-from typing import Tuple
+from typing import Tuple, List
 
 from databases import Database
 from fastapi import Depends, FastAPI, HTTPException, status
@@ -33,25 +33,25 @@ async def get_post_or_404(
     return PostDB(**raw_post)
 
 
-# async def pagination(skip: int = 0, limit: int = 10) -> Tuple[int, int]:
-#     capped_limit = min(100, limit)
-#     return (skip, capped_limit)
+async def pagination(skip: int = 0, limit: int = 10) -> Tuple[int, int]:
+    capped_limit = min(100, limit)
+    return (skip, capped_limit)
 
 
-# @app.on_event('startup')
-# async def startup():
-#     """Database connection code runs on startup
-#     """
-#     await database.connect()
-#     metadata.create_all(sqlalchemy_engine)
+@app.on_event('startup')
+async def startup():
+    """Database connection code runs on startup
+    """
+    await database.connect()
+    metadata.create_all(sqlalchemy_engine)
 
 
-# @app.on_event('shutdown')
-# async def shutdown():
-#     """Database disconnection code runs on shutdown
+@app.on_event('shutdown')
+async def shutdown():
+    """Database disconnection code runs on shutdown
 
-#     """
-#     await database.disconnect()
+    """
+    await database.disconnect()
 
 
 @app.get(
@@ -97,105 +97,23 @@ async def create_post(
 
     return _post_db
 
-# @app.get(
-#     "/posts",
-#     name="Get all posts",
-#     description="Endpoint to get all posts",
-#     response_model=list[PostDB],
-#     tags=['Posts']
-# )
-# async def list_posts(
-#     _pagination: Tuple[int, int] = Depends(pagination),
-#     _database: Database = Depends(get_database)
-# ) -> list[PostDB]:
-#     """List all posts from the database
 
-#     Args:
-#         _pagination (Tuple[int, int], optional): This defined pagination for the data fetched. Defaults to Depends(pagination).
-#         _database (Database, optional): The database connection. Defaults to Depends(get_database).
+@app.get("/posts")
+async def list_posts(
+    pagination: Tuple[int, int] = Depends(pagination),
+    database: AsyncIOMotorDatabase = Depends(get_database)
+) -> List[PostDB]:
+    """A list posts endpoint
 
-#     Returns:
-#         list[PostDB]: A list of all the posts from the database
-#     """
-#     skip, limit = _pagination
-#     select_query = posts.select().offset(skip).limit(limit)
-#     rows = await _database.fetch_all(select_query)
+    Args:
+        pagination (Tuple[int, int], optional): The pagination parameters. Defaults to (0, 10).
 
-#     results = [PostDB(**row) for row in rows]
+    Returns:
+        List[PostDB]: A list of posts
+    """
+    skip, limit = pagination
+    query = database["dumb_blog"].find({}, skip=skip, limit=limit)
 
-#     return results
+    results = [PostDB(**raw_post) async for raw_post in query]
 
-
-# @app.get(
-#     "/posts/{id}",
-#     name="Get a post",
-#     description="Endpoint to get a post",
-#     response_model=PostDB,
-#     tags=['Posts']
-# )
-# async def get_post(
-#     _post: PostDB = Depends(get_post_or_404)
-# ) -> PostDB:
-#     """Get a post from the database
-
-#     Args:
-#         post (PostDB, optional): The post to be fetched. Defaults to Depends(get_post_or_404).
-
-#     Returns:
-#         PostDB: The post fetched from the database
-#     """
-#     return _post
-
-
-# @app.patch(
-#     "/posts/{id}",
-#     name="Update a post",
-#     description="Endpoint to update a post",
-#     tags=['Posts']
-# )
-# async def update_post(
-#     _post_update: PostPartialUpdate,
-#     _post: PostDB = Depends(get_post_or_404),
-#     _database: Database = Depends(get_database)
-# ) -> PostDB:
-#     """The Post Update endpoint
-
-#     Args:
-#         _post_update (PostPartialUpdate): _description_
-#         _post (PostDB, optional): _description_. Defaults to Depends(get_post_or_404).
-#         _database (Database, optional): _description_. Defaults to Depends(get_database).
-
-#     Returns:
-#         PostDB: _description_
-#     """
-#     _update_query = (
-#         _posts.update()
-#         .where(_posts.c.id == _post.id)
-#         .values(_post_update.dict(exclude_unset=True))
-#     )
-
-#     _post_id = await _database.execute(_update_query)
-#     _post_db = await get_post_or_404(_post_id, _database)
-
-#     return _post_db
-
-
-# @app.delete(
-#     "/posts/{id}",
-#     name="Delete a post",
-#     tags=['Posts'],
-#     status_code=status.HTTP_204_NO_CONTENT,
-#     description="Endpoint to delete a post"
-# )
-# async def delete_post(
-#     _post: PostDB = Depends(get_post_or_404),
-#     _database: Database = Depends(get_database)
-# ):
-#     """Delete a post from the database
-
-#     Args:
-#         post (PostDB, optional): The post to be deleted. Defaults to Depends(get_post_or_404).
-#         database (Database, optional): The database connection. Defaults to Depends(get_database).
-#     """
-#     delete_query = posts.delete().where(posts.c.id == _post.id)
-#     await _database.execute(delete_query)
+    return results
